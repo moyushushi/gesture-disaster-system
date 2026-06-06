75860f9fcf32209a3a9882fccac729ae8b086d2f
你的名字 <@example.com>
2026-06-06 12:53:16 +0800

    最终版v0.1.1

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig(({ mode }) => {
-   console.log(`当前构建模式: ${mode}`)
-   return {
-  plugins: [vue()],
-     resolve: {
-       alias: {
-         '@': path.resolve(__dirname, './src')
-       }
-     },
-  server: {
-    port: 5173,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsDir: 'static',
-  },
-  base: '/gesture-disaster-system/'
-}
+    console.log(`当前构建模式: ${mode}`)
+    return {
+        plugins: [
+            vue(),
+            viteStaticCopy({
+                targets: [
+                    {
+
+                        src: 'node_modules/@supermap/iclient3d-webgl/Cesium/**/*',
+                        dest: 'cesium'
+                    }
+                ]
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src')
+            }
+        },
+        server: {
+            port: 5173,
+            open: true
+        },
+        build: {
+            outDir: 'dist',
+            emptyOutDir: true,
+            assetsDir: 'static',
+        },
+        base: '/gesture-disaster-system/'
+    }
 })
